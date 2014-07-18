85a507814e948143a5a54bd2e6b521d43c03c7f2
Nathan Baum <n@p12a.org.uk>
2014-07-18 22:31:20 +0100

    Update link helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,9 @@ module ApplicationHelper
   
   def convert_options_to_data_attributes (options, html_options)
     url = url_for(options)
-    if request.path == url
-      html_options ||= {}
+    html_options ||= {}
+    if request.path == url or (html_options[:exact] != true and request.path.start_with?(url))
       html_options["class"] = "active"
-    elsif request.path.start_with?(url)
-      html_options ||= {}
-      html_options["class"] = "active-under"
     end
     super(options, html_options)
   end
