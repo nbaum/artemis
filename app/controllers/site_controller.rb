379723b8f1ed9aac02a8c63fb42255bfea2e8778
Nathan Baum <n@p12a.org.uk>
2014-07-18 22:14:15 +0100

    Move sample file.

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,5 +1,9 @@
 class SiteController < ApplicationController
 
+  def root
+    render :sample
+  end
+
   def error
     @fake = env["action_dispatch.exception"].nil?
     render "site/errors/#{params[:status]}"
